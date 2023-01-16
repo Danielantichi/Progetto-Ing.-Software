0c618400c8964bb1613a97bd9ccc6cd6a360eea5
Danielantichi <s1092876@studenti.univpm.it>
2023-01-16 15:17:34 +0100

    a

--- pythonProject2/entrata/model/Entrata.py
+++
@@ -1,4 +0,0 @@
-class Entrata():
-    def __init__(self, nome, prezzo):
-        self.nome = nome
-        self.prezzo = prezzo
--- pythonProject2/prenotazione/model/Prenotazione.py
+++
@@ -1,6 +0,0 @@
-class Prenotazione():
-    def __init__(self, id, cliente, attivita, data):
-        self.id = id
-        self.cliente = cliente
-        self.attivita = attivita
-        self.data = data
